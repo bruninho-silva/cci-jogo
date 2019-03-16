c6d58fdaf5697d0ac1da45f7113b8dd9bc799987
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-03-16 18:57:58 -0300

    Arrumando os conflitos e recuperado os layouts

---
+++ Assets/Scripts/swapSceneTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class swapSceneTime : MonoBehaviour
+{
+    [Header("Cenarios")]
+    [SerializeField]
+    public string digiteACena;
+    [HideInInspector]
+	public float tempo;
+
+    void Update()
+    {
+        tempo +=Time.deltaTime;
+		if(tempo >= 1.5){
+            SceneManager.LoadScene(digiteACena);
+        }
+    }
+}
