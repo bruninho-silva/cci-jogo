a50f26706d6a08c4367c908b30a7d025ca2b2e58
Bruno <bruno.depaula2006@hotmail.com>
2019-03-30 11:53:48 -0300

    Adicionado movimentacao no personagem

--- Assets/Scenes/movimentacao.cs
+++ Assets/Scenes/movimentacao.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class movimentacao : MonoBehaviour
 {
+    [SerializeField]
+    private int direcao = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class movimentacao : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mover();
     }
+public void direita(){
+direcao = 2;
+}
+
+public void esquerda(){
+direcao = -2;
+}
+
+public void parar(){
+direcao = 0;
+}
+
+public void mover(){
+    transform.Translate(direcao * Time.deltaTime,0,0);
+}
 }
