c1ff6f3a780fc785b74c8e3a3990722fd36f4d9f
Bruno <bruno.depaula2006@hotmail.com>
2019-04-07 12:45:52 -0300

    Adicionado camera para seguir personagem

---
+++ Assets/Scenes/CameraSegue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSegue : MonoBehaviour
+{
+
+    public float dampTime = 0.15f; //valor da suavização
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;// ponto
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
+            Vector3 delta = target.position = GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+        }  
+
+        
+    }
+}
---
+++ Assets/Scenes/CameraSegueTutorialMetalSnug.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSegueTutorialMetalSnug : MonoBehaviour
+{
+
+    public GameObject player;
+    public float camVel = 0.25f;
+    private bool seguePlayer;
+    public Vector3 ultimoAlvoPos;
+    public Vector3 velAtual;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        seguePlayer = true;
+        ultimoAlvoPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (seguePlayer)
+        {
+            if (player.transform.position.x >= transform.position.x)
+            {
+                Vector3 novaCamPos = Vector3.SmoothDamp(transform.position, player.transform.position, ref velAtual, camVel);
+                transform.position = new Vector3(novaCamPos.x, novaCamPos.y, transform.position.z);
+                ultimoAlvoPos = player.transform.position;
+            }
+        }
+    }
+}
