4ba3619164f0c88efd26b9b8423b3bc5e93c4f9a
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-04-07 01:18:31 -0300

    Personagem andando com animacao

--- Assets/Scenes/movimentacao.cs
+++ Assets/Scenes/movimentacao.cs
@@ -6,10 +6,12 @@ public class movimentacao : MonoBehaviour
 {
     [SerializeField]
     private int direcao = 0;
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {    
+
     }
 
     // Update is called once per frame
@@ -17,19 +19,19 @@ public class movimentacao : MonoBehaviour
     {
         mover();
     }
-public void direita(){
-direcao = 2;
-}
+    public void direita(){
+        direcao = 2;
+    }
 
-public void esquerda(){
-direcao = -2;
-}
+    public void esquerda(){
+        direcao = -2;
+    }
 
-public void parar(){
-direcao = 0;
-}
+    public void parar(){
+        direcao = 0;
+    }
 
-public void mover(){
-    transform.Translate(direcao * Time.deltaTime,0,0);
-}
+    public void mover(){
+        transform.Translate(direcao * Time.deltaTime,0,0);
+    }
 }
---
+++ Assets/Scenes/movimentacao1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimentacao1 : MonoBehaviour
+{
+    [HideInInspector]
+    private int direcao = 0;
+
+    [SerializeField]
+    public int velocidade = 6;
+    // Animator = Responsavel por Habilitar ou Desabilidar a movimentação do Personagem.
+    public Animator anins;
+    // Flip = Resposanvel para inverter a posição do Personagem.
+    public SpriteRenderer posicao_inverti;
+
+    // Atualização é chamada uma vez por quadro
+    void Update()
+    {
+        mover();
+    }
+
+    // Ira habilitar Andar para DIRETA
+    public void direita(){
+        anins.SetBool("Parado", false);
+        anins.SetBool("Andando", true);
+        posicao_inverti.flipX = false;
+        direcao = velocidade;
+    }
+
+    // Ira habilitar Andar para ESQUERDA
+    public void esquerda(){
+        anins.SetBool("Parado", false);
+        anins.SetBool("Andando", true);
+        posicao_inverti.flipX = true;
+        direcao = -velocidade;
+    }
+
+    // Ira desabilitar o Andar
+    public void parar(){
+        direcao = 0;
+        anins.SetBool("Parado", true);
+        anins.SetBool("Andando", false);
+    }
+
+    // De acordo com a direcao, este valor sera atribuido no x, fazendo o personagem andar.
+    public void mover(){
+        transform.Translate(direcao * Time.deltaTime,0,0);
+    }
+}
