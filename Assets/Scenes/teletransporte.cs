857f01819a15997ac9faa1cba4149f931fb50a33
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-04-07 05:59:44 -0300

    Criando teletransporte

---
+++ Assets/Scenes/teletransporte.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teletransporte : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject button;
+
+    public GameObject personagem;
+    public GameObject currentPoint;
+
+    public bool abrir = false;
+
+    public GameObject[] teste;
+    int index;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        index = Random.Range(0, teste.Length);
+        currentPoint = teste[index];
+    }
+
+    void OnCollisionEnter2D(Collision2D porta)
+    {
+
+        if (porta.gameObject.CompareTag("porta"))
+        {
+            personagem.transform.position = currentPoint.transform.position;
+            abrir = true;
+        } else
+        {
+            abrir = false;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (Event.current.button == 1)
+        {
+            if (Event.current.type == EventType.MouseDown)
+            {
+                print("funciona");
+            }
+        }
+    }
+
+}
+
