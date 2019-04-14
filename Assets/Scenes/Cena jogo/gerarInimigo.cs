100f0efaca0793f267d39ea4c1906134adb5fe92
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-04-14 13:46:22 -0300

    Fundo e destroir o ladrao

---
+++ Assets/Scenes/Cena jogo/gerarInimigo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gerarInimigo : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            print("02");
+            ScoreTextScript.coinAmount += 20;
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
