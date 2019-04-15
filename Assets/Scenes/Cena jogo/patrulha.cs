94954f695f747637af22f78f74012c44f62d6671
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-04-14 23:54:39 -0300

    Adicionado gato

---
+++ Assets/Scenes/Cena jogo/patrulha.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrulha : MonoBehaviour
+{
+    public Transform DetectaChao;
+    public float distancia = 3;
+    public bool olhandoParaDireita;
+    public float velocidade = 4;
+
+    void Start()
+    {
+        olhandoParaDireita = true;
+    }
+
+    void Update()
+    {
+        Patrulha();
+    }
+
+    public void Patrulha()
+    {
+        transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(DetectaChao.position, Vector2.down, distancia);
+        if (groundInfo.collider == false)
+        {
+            if (olhandoParaDireita == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                olhandoParaDireita = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                olhandoParaDireita = true;
+            }
+        }
+    }
+}
