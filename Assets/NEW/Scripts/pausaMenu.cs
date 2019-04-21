bd9a398588660ba4477220d69d33bdb1aad2e234
Marcelo Bueno <marcelobueno25@hotmail.com>
2019-04-20 21:41:24 -0300

    Adicionado menu

---
+++ Assets/NEW/Scripts/pausaMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausaMenu : MonoBehaviour
+{
+
+    public GameObject PausaUI;
+
+    private bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PausaUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused)
+        {
+            PausaUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if(!paused)
+        {
+            PausaUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+    public void AtivarMenu()
+    {
+        paused = !paused;
+    }
+}
